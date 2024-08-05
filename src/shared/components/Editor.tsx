0bf2ae0facec5b79d7d326d043b9e1730909786e
WindyGhoster <jsnice0902@naver.com>
2024-08-05 13:21:11 +0900

    dev-0.0.2
     Added focus position system

--- src/shared/components/Editor.tsx
+++ src/shared/components/Editor.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ChangeEventHandler, CSSProperties, useCallback, useEffect, useState } from "react";
+import React, { ChangeEventHandler, CSSProperties, useCallback, useEffect, useState } from "react";
 import { TabBox } from "./TabBox";  
 
 
@@ -16,32 +16,37 @@ class BlockData {
         this.data = "";
     }
 }
-
+type FocusData = {
+    index: number, 
+    selection: number
+}
 class Document {
     id: string;
     blocks: BlockData[];
-    focus: number;
+    focus: FocusData;
     constructor(id: string) {
         this.id = id;
         this.blocks = [new BlockData(BlockType.Text)];
-        this.focus = 0;
+        this.focus = {index:0, selection:0};
     }
     insertBlock = (block: BlockData, index: number) => {
         this.blocks = [...this.blocks.slice(0, index), block, ...this.blocks.slice(index, this.blocks.length)];
     }
 }
 
+
 function Block({
     className, style,
-    id, index, datas, fnAddBlock, fnChangeFocus
+    id, index, data,
+    fnAddBlock, fnSetFocus
 } : {
     className?: string | undefined, style?: CSSProperties | undefined
-    id: string, index: number, datas: BlockData[], fnAddBlock: (index: number) => void, fnChangeFocus: (delta: number) => void
+    id: string, index: number, data: BlockData
+    fnAddBlock: (index: number) => void, fnSetFocus: (value: FocusData) => void
 }) {
     
     
-    
-    if (datas[index].type == BlockType.Separator)
+    if (data.type == BlockType.Separator)
         return <hr className="mt-[4px] mb-[3px] border-y-1" />
         // return <div className="mt-[14px] mb-[15px] h-[10px] bg-slate-500"></div>
 
@@ -63,42 +68,53 @@ function Block({
         // let height = textarea.scrollHeight; // 높이
         // textarea.style.height = `${height}px`;
         var height = `${textarea.value.split("\n").length*24+6}px`;
-        var block = document.getElementById(`Block-${id}-${index}`) as HTMLTextAreaElement | null;
+        var block = window.document.getElementById(`Block-${id}-${index}`) as HTMLTextAreaElement | null;
         if (block) block.style.height = height;
         textarea.style.height = height;
     }
     function onKeyDown(event: React.KeyboardEvent<HTMLDivElement>) {
-        if (event.key == "Enter" && !event.shiftKey) { fnAddBlock(index+1); event.preventDefault() }
-        var textarea = document.getElementById(`Input-${id}-${index}`) as HTMLTextAreaElement | null;
+        if (event.key == "Enter" && !event.shiftKey) { fnAddBlock(index+1); event.preventDefault(); return; }
+        var textarea = window.document.getElementById(`Input-${id}-${index}`) as HTMLTextAreaElement | null;
         var selectionStart = textarea?.selectionStart ?? -1;
         var selectionEnd = textarea?.selectionEnd ?? -1;
         var firstLineEnd = textarea?.value.indexOf("\n") ?? -1;
         var lastLineStart = textarea?.value.lastIndexOf("\n") ?? -1;
-        if ((firstLineEnd  == -1 || selectionStart <= firstLineEnd) && event.key == "ArrowUp"  ) fnChangeFocus(-1);
-        if ((lastLineStart == -1 || selectionEnd   > lastLineStart) && event.key == "ArrowDown") fnChangeFocus( 1);
-        if (selectionStart == 0 && event.key == "ArrowLeft") fnChangeFocus(-1);
-        if (selectionEnd == (textarea?.value.length ?? -1) && event.key == "ArrowRight") fnChangeFocus(1);
+        if ((firstLineEnd  == -1 || selectionStart <= firstLineEnd) && event.key == "ArrowUp"  ) { fnSetFocus({index:index-1, selection:selectionStart}); event.preventDefault(); return; };
+        if ((lastLineStart == -1 || selectionEnd   > lastLineStart) && event.key == "ArrowDown") { fnSetFocus({index:index+1, selection:selectionStart}); event.preventDefault(); return; };
+        if (selectionStart == 0 && event.key == "ArrowLeft") { fnSetFocus({index:index-1, selection:selectionStart}); event.preventDefault(); return; };
+        if (selectionEnd == (textarea?.value.length ?? -1) && event.key == "ArrowRight") { fnSetFocus({index:index+1, selection:selectionStart}); event.preventDefault(); return; };
+    }
+    function onKeyUp(event: React.KeyboardEvent<HTMLDivElement>) {
+        var textarea = window.document.getElementById(`Input-${id}-${index}`) as HTMLTextAreaElement | null;
+        var selectionStart = textarea?.selectionStart ?? -1;
+        fnSetFocus({index:index, selection:selectionStart})
     }
     var [text, setText] = useState("");
     function executeText(text: string) {
-        datas[index].data = text
-        setText(datas[index].data)
+        data.data = text
+        setText(data.data)
         if (text == "---") {
-            datas[index].type = BlockType.Separator
-            datas[index].data = "";
-            fnChangeFocus(1);
+            data.type = BlockType.Separator
+            data.data = "";
+            fnSetFocus({index:index+1, selection:0});
         }
     }
     useEffect(() => {
-        setText(datas[index].data)
-        setTimeout(() => fixHeight(document.getElementById(`Input-${id}-${index}`)), 5)
-    }, [index, datas])
-    var onChange: ChangeEventHandler<HTMLTextAreaElement> = useCallback(event => executeText(event.target.value), [index, datas]);
-    return <div id={`Block-${id}-${index}`} className={className} style={style} onKeyDown={onKeyDown}>
+        setText(data.data)
+        setTimeout(() => {
+            var target = window.document.getElementById(`Input-${id}-${index}`);
+            if (target) fixHeight(target as HTMLTextAreaElement);
+        }, 5)
+    }, [index, data])
+    var onChange: ChangeEventHandler<HTMLTextAreaElement> = useCallback(event => executeText(event.target.value), [index, data]);
+    return <div id={`Block-${id}-${index}`} className={className} style={style} onKeyDown={onKeyDown} onKeyUp={onKeyUp}>
         <textarea 
                 id={`Input-${id}-${index}`} className={"placeholder:text-white focus:placeholder:text-gray-400 overflow-hidden resize-none outline-none"} style={{height:"30px", width:"100%", border:0}} 
                 value={text} onChange={onChange} placeholder={"Write down somthing surprising!"}
-                onInput={e => fixHeight(e.target)}/>
+                onInput={() => {
+                    var target = window.document.getElementById(`Input-${id}-${index}`);
+                    if (target) fixHeight(target as HTMLTextAreaElement);
+                }}/>
     </div>
 }
 
@@ -107,6 +123,8 @@ export default function Editor({className, style}: {className?: string | undefin
     var documentData = new Document("haru");
 
     var [toolNumber, setToolNumber] = useState(-1);
+
+    
     var [docmuentBlocks, setDocumentBlocks] = useState(documentData.blocks);
     var [docmuentFocus, setDocumentFocus] = useState(documentData.focus);
 
@@ -114,27 +132,35 @@ export default function Editor({className, style}: {className?: string | undefin
         documentData.blocks = docmuentBlocks;
         documentData.focus = docmuentFocus;
     }
+    useEffect(
+        saveDocument,
+        [docmuentBlocks, docmuentFocus]
+    )
 
     function fnAddBlock(index: number) {
         var data: BlockData = new BlockData(BlockType.Text);
         setDocumentBlocks([...docmuentBlocks.slice(0, index), data, ...docmuentBlocks.slice(index, docmuentBlocks.length)])
-        setDocumentFocus(index);
+        setDocumentFocus({index: index, selection:docmuentFocus.selection});
     }
 
     useEffect(
         () => {
             function focus() {
-                var element = document.getElementById(`Input-${documentData.id}-${docmuentFocus}`);
-                if (element) element.focus();
+                var element = document.getElementById(`Input-${documentData.id}-${docmuentFocus.index}`) as HTMLTextAreaElement;
+                if (element) {
+                    element.setSelectionRange(docmuentFocus.selection, docmuentFocus.selection);
+                    element.focus();
+                }
                 else setTimeout(focus, 10);
             }
-            setTimeout(focus, 10);
+            focus()
         }, [docmuentFocus]
-    )
+    );
 
     return <div className={"flex flex-col overflow-hidden " + className} style={style}>
-        <TabBox select={toolNumber} onSelect={setToolNumber} tabnames={["File", "Edit"]}>
+        <TabBox select={toolNumber} onSelect={setToolNumber} tabnames={["File", "Edit", "View"]}>
             <div>FileMenu</div>
+            <div>EditMenu</div>
             <div>EditMenu</div>
         </TabBox>
         
@@ -148,14 +174,14 @@ export default function Editor({className, style}: {className?: string | undefin
 
                                     id={documentData.id}
                                     index={index}
-                                    datas={docmuentBlocks}
+                                    data={block}
                                     fnAddBlock={fnAddBlock}
-                                    fnChangeFocus={delta => setDocumentFocus(docmuentFocus+delta)}
+                                    fnSetFocus={setDocumentFocus}
                                     
                                 />
                             ))}
             </div>
         </div>
-        {/* <button onClick={() => {saveDocument(); console.log(documentData.blocks)}}>Print</button> */}
+        <button onClick={() => {saveDocument(); console.log(documentData)}}>Print</button>
     </div>
 }
